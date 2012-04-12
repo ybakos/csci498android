5b688dc02b3722fa704afe788e3c2be9f095f353
Yong Bakos <ybakos@humanoriented.com>
2012-04-12 15:54:25 -0600

    Personalizing hello message.

--- HelloAndroid/src/edu/mines/csci498/hello/HelloAndroidActivity.java
+++ HelloAndroid/src/edu/mines/csci498/hello/HelloAndroidActivity.java
@@ -2,12 +2,15 @@ package edu.mines.csci498.hello;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HelloAndroidActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        TextView tv = new TextView(this);
+        tv.setText("Hello, Yong!");
+        setContentView(tv);
     }
 }
