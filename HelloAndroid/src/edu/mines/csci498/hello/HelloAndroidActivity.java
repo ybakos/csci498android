4862c31542db2b3b6d7e9f71bc7915a82b2d4c84
Yong Bakos <ybakos@humanoriented.com>
2012-04-12 15:52:41 -0600

    Initial commit.

---
+++ HelloAndroid/src/edu/mines/csci498/hello/HelloAndroidActivity.java
@@ -0,0 +1,13 @@
+package edu.mines.csci498.hello;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# CSCI498 Mobile Device Programming w/ Android
+## Fall 2012
+### Yong Joseph Bakos
+
+This repository contains my project work for CSCI498 at Mines.
+
