8a7d6ef468bd7c3891fb0235b98f93e72fbfdb63
Yong Bakos <ybakos@humanoriented.com>
2012-04-12 16:04:54 -0600

    Using xml-driven layout per tutorial.

--- HelloAndroid/src/edu/mines/csci498/hello/HelloAndroidActivity.java
+++ HelloAndroid/src/edu/mines/csci498/hello/HelloAndroidActivity.java
@@ -2,15 +2,12 @@ package edu.mines.csci498.hello;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class HelloAndroidActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView tv = new TextView(this);
-        tv.setText("Hello, Yong!");
-        setContentView(tv);
+        setContentView(R.layout.main);
     }
 }
