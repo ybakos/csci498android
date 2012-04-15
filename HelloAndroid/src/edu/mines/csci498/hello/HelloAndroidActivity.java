4f3bf81797254631f7817b567064564fed280d63
Yong Bakos <ybakos@humanoriented.com>
2012-04-15 13:59:26 -0600

    Adding simple view, button and onClick handler per BCG chapter 7.

--- HelloAndroid/src/edu/mines/csci498/hello/HelloAndroidActivity.java
+++ HelloAndroid/src/edu/mines/csci498/hello/HelloAndroidActivity.java
@@ -2,12 +2,29 @@ package edu.mines.csci498.hello;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import java.util.Date;
 
-public class HelloAndroidActivity extends Activity {
-    /** Called when the activity is first created. */
+public class HelloAndroidActivity extends Activity implements View.OnClickListener {
+
+	Button btn;
+	
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        btn = new Button(this);
+        btn.setOnClickListener(this);
+        updateTime();
+        setContentView(btn);
     }
+    
+    public void onClick(View view) {
+    	updateTime();
+    }
+    
+    public void updateTime() {
+    	btn.setText(new Date().toString());
+    }
+
 }
