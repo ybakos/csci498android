8b8216c64391b91852a20aab486bdb88500e9a6c
Yong Bakos <ybakos@humanoriented.com>
2012-04-15 11:48:10 -0600

    Initial commit of LunchList app.

---
+++ LunchList/src/edu/mines/csci498/ybakos/lunchlist/LunchListActivity.java
@@ -0,0 +1,13 @@
+package edu.mines.csci498.ybakos.lunchlist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LunchListActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
