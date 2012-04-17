02e8372bdd38669c86e1f9c7f105fc1a72043f79
Yong Bakos <ybakos@humanoriented.com>
2012-04-16 22:10:39 -0600

    Adding Restaurant model.

---
+++ LunchList/src/edu/mines/csci498/ybakos/lunchlist/Restaurant.java
@@ -0,0 +1,24 @@
+package edu.mines.csci498.ybakos.lunchlist;
+
+public class Restaurant {
+
+	private String name = "";
+	private String address = "";
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
