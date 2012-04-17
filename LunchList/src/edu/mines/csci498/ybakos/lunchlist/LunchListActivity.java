94919f163958079cfd409d34e8fda4f7b67abb76
Yong Bakos <ybakos@humanoriented.com>
2012-04-16 23:46:19 -0600

    Adding Activity event bindings to Restaurant model.

--- LunchList/src/edu/mines/csci498/ybakos/lunchlist/LunchListActivity.java
+++ LunchList/src/edu/mines/csci498/ybakos/lunchlist/LunchListActivity.java
@@ -2,12 +2,31 @@ package edu.mines.csci498.ybakos.lunchlist;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LunchListActivity extends Activity {
-    /** Called when the activity is first created. */
+    
+	Restaurant r = new Restaurant();
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button save = (Button)findViewById(R.id.save);
+        save.setOnClickListener(onSave);
     }
+    
+    private View.OnClickListener onSave = new View.OnClickListener() {
+    		public void onClick(View v) {
+    			EditText name = (EditText)findViewById(R.id.name);
+    			EditText address = (EditText)findViewById(R.id.address);
+    			r.setName(name.getText().toString());
+    			r.setAddress(address.getText().toString());
+    		}
+    	};
+    
+    
 }
