5081852a771aec4fb19e9bbe8057921dcd698ac1
Yong Bakos <ybakos@humanoriented.com>
2012-09-30 11:41:06 -0600

    Coming back to shore. Replacing custom implementation with those from tutorial to be more in sync with book code. Adding row.xml for all restuarants. Deprecating custom work in activity. End of APT 11 Database persistence.

--- LunchList/src/edu/mines/csci498/ybakos/lunchlist/LunchListActivity.java
+++ LunchList/src/edu/mines/csci498/ybakos/lunchlist/LunchListActivity.java
@@ -3,266 +3,149 @@ package edu.mines.csci498.ybakos.lunchlist;
 import android.app.TabActivity;
 import android.content.Context;
 import android.database.Cursor;
-import android.widget.*;
-import android.view.*;
 import android.os.Bundle;
-import android.os.SystemClock;
-import android.graphics.Typeface;
-import android.util.Log;
-import java.util.*;
-import java.util.concurrent.atomic.AtomicBoolean;
+import android.view.*;
+import android.widget.*;
 
 @SuppressWarnings("deprecation")
 public class LunchListActivity extends TabActivity {
+	Cursor model;
+	RestaurantAdapter adapter;
+	EditText name;
+	EditText address;
+	EditText notes;
+	RadioGroup types;
+	RestaurantHelper helper;
 
-	Cursor restaurants;
-	RestaurantsAdapter restaurantsAdapter;
-	List<String> addresses;
-	ArrayAdapter<String> addressesAdapter;
-	EditText nameField;
-	AutoCompleteTextView addressField;
-	RadioGroup restaurantTypesGroup;
-	EditText notesField;
-	Restaurant currentRestaurant; // for Toast
-	int progress; // for progress bar
-	AtomicBoolean isActive = new AtomicBoolean(true);
-	RestaurantHelper restaurantHelper;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		helper = new RestaurantHelper(this);
+
+		name = (EditText) findViewById(R.id.name);
+		address = (EditText) findViewById(R.id.address);
+		notes = (EditText) findViewById(R.id.notes);
+		types = (RadioGroup) findViewById(R.id.restaurantTypes);
+
+		Button save = (Button) findViewById(R.id.save);
+
+		save.setOnClickListener(onSave);
+
+		ListView list = (ListView) findViewById(R.id.restaurants);
+
+		model = helper.getAll();
+		startManagingCursor(model);
+		adapter = new RestaurantAdapter(model);
+		list.setAdapter(adapter);
+
+		TabHost.TabSpec spec = getTabHost().newTabSpec("tag1");
+
+		spec.setContent(R.id.restaurants);
+		spec.setIndicator("List", getResources().getDrawable(R.drawable.icon_list));
+		getTabHost().addTab(spec);
+
+		spec = getTabHost().newTabSpec("tag2");
+		spec.setContent(R.id.details);
+		spec.setIndicator("Details", getResources().getDrawable(R.drawable.icon_restaurant));
+		getTabHost().addTab(spec);
+
+		getTabHost().setCurrentTab(0);
+
+		list.setOnItemClickListener(onListClick);
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		helper.close();
+	}
+
+	private View.OnClickListener onSave = new View.OnClickListener() {
+		public void onClick(View v) {
+			String type = null;
+			switch (types.getCheckedRadioButtonId()) {
+			case R.id.sit_down:
+				type = "sit_down";
+				break;
+			case R.id.take_out:
+				type = "take_out";
+				break;
+			case R.id.delivery:
+				type = "delivery";
+				break;
+			}
+			helper.insert(name.getText().toString(), address.getText().toString(),
+						  type, notes.getText().toString());
+			model.requery();
+		}
+	};
+
+	private AdapterView.OnItemClickListener onListClick = new AdapterView.OnItemClickListener() {
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+			model.moveToPosition(position);
+			name.setText(helper.getName(model));
+			address.setText(helper.getAddress(model));
+			notes.setText(helper.getNotes(model));
+
+			if (helper.getType(model).equals("sit_down")) {
+				types.check(R.id.sit_down);
+			} else if (helper.getType(model).equals("take_out")) {
+				types.check(R.id.take_out);
+			} else {
+				types.check(R.id.delivery);
+			}
+
+			getTabHost().setCurrentTab(1);
+		}
+	};
+
+	class RestaurantAdapter extends CursorAdapter {
 		
-	// A customized ArrayAdapter to customize it's getView behavior.
-	class RestaurantsAdapter extends CursorAdapter {
-
-		RestaurantsAdapter(Cursor c) {
+		RestaurantAdapter(Cursor c) {
 			super(LunchListActivity.this, c);
 		}
 
 		@Override
-		public void bindView(View row, Context context, Cursor cursor) {
-			RestaurantHolder viewHolder = (RestaurantHolder) row.getTag();
-			viewHolder.populateFrom(cursor, restaurantHelper);
-		}
-		
-		@Override
-		public View newView(Context context, Cursor cursor, ViewGroup parent) {
-			LayoutInflater inflater = getLayoutInflater();
-			View row;
-			if (cursor.getString(3) == "delivery") {
-				row = inflater.inflate(R.layout.row_delivery, parent, false);
-			} else if (cursor.getString(3) == "take_out") {
-				row = inflater.inflate(R.layout.row_take_out, parent, false);
-			} else {
-				row = inflater.inflate(R.layout.row_sit_down, parent, false);
-			}
-			RestaurantHolder viewHolder = new RestaurantHolder(row);
-			row.setTag(viewHolder);
-			return row;
+		public void bindView(View row, Context ctxt, Cursor c) {
+			RestaurantHolder holder = (RestaurantHolder) row.getTag();
+
+			holder.populateFrom(c, helper);
 		}
 
+		@Override
+		public View newView(Context ctxt, Cursor c, ViewGroup parent) {
+			LayoutInflater inflater = getLayoutInflater();
+			View row = inflater.inflate(R.layout.row, parent, false);
+			RestaurantHolder holder = new RestaurantHolder(row);
+			row.setTag(holder);
+			return (row);
+		}
 	}
 
 	static class RestaurantHolder {
 
-		private TextView name;
-		private TextView address;
+		private TextView name = null;
+		private TextView address = null;
+		private ImageView icon = null;
 
 		RestaurantHolder(View row) {
-			name = (TextView)row.findViewById(R.id.name);
-			address = (TextView)row.findViewById(R.id.address);
+			name = (TextView) row.findViewById(R.id.name);
+			address = (TextView) row.findViewById(R.id.address);
+			icon = (ImageView) row.findViewById(R.id.icon);
 		}
 
 		void populateFrom(Cursor c, RestaurantHelper helper) {
 			name.setText(helper.getName(c));
 			address.setText(helper.getAddress(c));
-		}
-
-	}
-
-	private Runnable longTask = new Runnable() {
-		public void run() {
-			for (int i = 0; i < 10000 && isActive.get(); i += 200) {
-				doSomeLongWork(200);
-			}
-			if (isActive.get()) {
-				runOnUiThread(new Runnable() {
-					public void run() {
-						setProgressBarVisibility(false);
-						progress = 0; // reset progress only if we are active (contrived)
-					}
-				});
-				runOnUiThread(new Runnable() {
-					public void run() {
-						Toast.makeText(LunchListActivity.this, "All done yo!", Toast.LENGTH_LONG).show();
-					}
-				});
+			if (helper.getType(c).equals("sit_down")) {
+				icon.setImageResource(R.drawable.icon_sit_down);
+			} else if (helper.getType(c).equals("take_out")) {
+				icon.setImageResource(R.drawable.icon_take_out);
+			} else {
+				icon.setImageResource(R.drawable.icon_delivery);
 			}
 		}
-	};
-	
-    private View.OnClickListener onSave = new View.OnClickListener() {
-		public void onClick(View v) {
-			currentRestaurant = new Restaurant();
-			String address = ((EditText)findViewById(R.id.address)).getText().toString(); // used twice
-			currentRestaurant.setName(((EditText)findViewById(R.id.name)).getText().toString());
-			currentRestaurant.setAddress(address);
-			currentRestaurant.setType(restaurantTypeFromRadioGroup((RadioGroup)findViewById(R.id.restaurantTypes)));
-			currentRestaurant.setNotes(((EditText)findViewById(R.id.notes)).getText().toString());
-			restaurantHelper.insert(currentRestaurant);
-			addressesAdapter.add(address);
-		}
-    };
-
-    private AdapterView.OnItemClickListener onListClick = new AdapterView.OnItemClickListener() {
-    	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-    		restaurants.moveToPosition(position);
-    		currentRestaurant.setName(restaurantHelper.getName(restaurants));
-    		currentRestaurant.setAddress(restaurantHelper.getAddress(restaurants));
-    		currentRestaurant.setNotes(restaurantHelper.getNotes(restaurants));
-    		currentRestaurant.setType(restaurantHelper.getType(restaurants));
-    		nameField.setText(currentRestaurant.getName());
-    		addressField.setText(currentRestaurant.getAddress());
-    		if (currentRestaurant.getType() == "sit_down") {
-    			restaurantTypesGroup.check(R.id.sit_down);
-    		} else if (currentRestaurant.getType() == "take_out") {
-    			restaurantTypesGroup.check(R.id.take_out);
-    		} else {
-    			restaurantTypesGroup.check(R.id.delivery);
-    		}
-    		getTabHost().setCurrentTab(1);
-    	}
-    };
-	
-	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_PROGRESS);
-        setContentView(R.layout.main);
-        restaurantHelper = new RestaurantHelper(this);
-        
-        instantiateFormElements();
-        setTypeFaces();
-        configureButton();
-        configureRestaurantsList();
-        configureAddressAutoComplete();
-        configureTabs();
-    }
-
-	@Override
-	public void onPause() {
-		super.onPause();
-		isActive.set(false);
 	}
-	
-	@Override
-	public void onResume() {
-		super.onResume();
-		isActive.set(true);
-		if (progress > 0) startWork();
-	}
-	
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		restaurantHelper.close();
-	}
-	
-	private void startWork() {
-		setProgressBarVisibility(true);
-		new Thread(longTask).start();
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		new MenuInflater(this).inflate(R.menu.option, menu);
-		return super.onCreateOptionsMenu(menu);
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		if (item.getItemId() == R.id.toast) {
-			String message = getString(R.string.option_unselected_message);
-			if (currentRestaurant != null) {
-				message = currentRestaurant.getNotes();
-			}
-			Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-			return true;
-		} else if (item.getItemId() == R.id.run) {
-			startWork();
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-	
-    private void instantiateFormElements() {
-    	nameField = (EditText)findViewById(R.id.name);
-        addressField = (AutoCompleteTextView)findViewById(R.id.address);
-        restaurantTypesGroup = (RadioGroup)findViewById(R.id.restaurantTypes);
-        notesField = (EditText)findViewById(R.id.name);
-    }
-
-    private void configureTabs() {
-        TabHost.TabSpec spec = getTabHost().newTabSpec("tag1");
-        spec.setContent(R.id.restaurants);
-        spec.setIndicator("List", getResources().getDrawable(R.drawable.icon_list));
-        getTabHost().addTab(spec);
-        spec = getTabHost().newTabSpec("tag2");
-        spec.setContent(R.id.details);
-        spec.setIndicator("Details", getResources().getDrawable(R.drawable.icon_restaurant));
-        getTabHost().addTab(spec);
-        getTabHost().setCurrentTab(0);
-    }
-
-    // Returns the restaurant type given a RadioGroup representing those types.
-    private String restaurantTypeFromRadioGroup(RadioGroup group) {
-    	if (group == null) return "";
-    	switch (group.getCheckedRadioButtonId()) {
-    		case R.id.sit_down:
-    			return "sit_down";
-    		case R.id.take_out:
-    			return "take_out";
-    		case R.id.delivery:
-    			return "delivery";
-    		default:
-    			return "";
-    	}
-    }
-
-    // For fun.
-    private void setTypeFaces() {
-        TextView field = (TextView)findViewById(R.id.name);
-        Typeface font = Typeface.createFromAsset(getAssets(), "Alphabits-Regular.ttf");
-        field.setTypeface(font);
-    }
-
-    // The main save button in this Activity.
-    private void configureButton() {
-        Button save = (Button)findViewById(R.id.save);
-        save.setOnClickListener(onSave);
-    }
-
-    // Creates and configures the ArrayAdapter for managing the ArrayList of Restaurant objects.
-    private void configureRestaurantsList() {
-    	restaurants = restaurantHelper.getAll();
-    	startManagingCursor(restaurants);
-        ListView restaurantList = (ListView)findViewById(R.id.restaurants);
-        restaurantsAdapter = new RestaurantsAdapter(restaurants);
-        restaurantList.setAdapter(restaurantsAdapter);
-        restaurantList.setOnItemClickListener(onListClick);
-    }
-
-    // Create and configure the ArrayAdapter for managing the ArrayList of address strings.
-    private void configureAddressAutoComplete() {
-    	addresses = new ArrayList<String>();
-        addressesAdapter = new ArrayAdapter<String>(this, R.layout.address_list_item_for_autocomplete, addresses);
-        addressField.setAdapter(addressesAdapter);
-    }
-    
-    private void doSomeLongWork(final int increment) {
-    	runOnUiThread(new Runnable() {
-    		public void run() {
-    			progress += increment;
-    			setProgress(progress);
-    		}
-    	});
-    	SystemClock.sleep(250); // simulating long task
-    }
-
 }
--- LunchList/src/edu/mines/csci498/ybakos/lunchlist/RestaurantHelper.java
+++ LunchList/src/edu/mines/csci498/ybakos/lunchlist/RestaurantHelper.java
@@ -2,56 +2,56 @@ package edu.mines.csci498.ybakos.lunchlist;
 
 import android.content.Context;
 import android.content.ContentValues;
-import android.database.SQLException;
 import android.database.Cursor;
-import android.database.sqlite.*;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase;
 
-public class RestaurantHelper extends SQLiteOpenHelper {
+class RestaurantHelper extends SQLiteOpenHelper {
 
 	private static final int SCHEMA_VERSION = 1;
-	Context context;
-	
+	private Context context;
+
 	public RestaurantHelper(Context context) {
-		super(context, context.getString(R.string.db_name), null, SCHEMA_VERSION);
+		super(context, context.getString(R.string.db_name), null,
+				SCHEMA_VERSION);
 		this.context = context;
 	}
-	
+
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL(context.getString(R.string.db_create_table));
 	}
-	
+
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		// not used yet
-		// make temp table, copy data, fix up real schema, copy data back. Egads!
+		// intentionally blank
 	}
-	
-	public void insert(Restaurant restaurant) {
-		ContentValues restaurantAttributes = new ContentValues();
-		restaurantAttributes.put("name", restaurant.getName());
-		restaurantAttributes.put("address", restaurant.getAddress());
-		restaurantAttributes.put("type", restaurant.getType());
-		restaurantAttributes.put("notes", restaurant.getNotes());
-		getWritableDatabase().insert("restaurants", "name", restaurantAttributes);
-	}
-	
+
 	public Cursor getAll() {
-		return getReadableDatabase().rawQuery(context.getString(R.string.db_select_restaurants), null);
+		return (getReadableDatabase().rawQuery(context.getString(R.string.db_select_restaurants), null));
 	}
-	
+
+	public void insert(String name, String address, String type, String notes) {
+		ContentValues cv = new ContentValues();
+		cv.put("name", name);
+		cv.put("address", address);
+		cv.put("type", type);
+		cv.put("notes", notes);
+		getWritableDatabase().insert("restaurants", "name", cv);
+	}
+
 	public String getName(Cursor c) {
 		return c.getString(1);
 	}
-	
+
 	public String getAddress(Cursor c) {
 		return c.getString(2);
 	}
-	
+
 	public String getType(Cursor c) {
 		return c.getString(3);
 	}
-	
+
 	public String getNotes(Cursor c) {
 		return c.getString(4);
 	}
