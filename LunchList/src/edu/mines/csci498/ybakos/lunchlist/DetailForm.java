28dbb5982c91ff3b170f53a72bd4962dd21eb582
Yong Bakos <ybakos@humanoriented.com>
2012-10-01 06:36:17 -0600

    Adding stub activity class and manifest entry.

---
+++ LunchList/src/edu/mines/csci498/ybakos/lunchlist/DetailForm.java
@@ -0,0 +1,12 @@
+package edu.mines.csci498.ybakos.lunchlist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetailForm extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
