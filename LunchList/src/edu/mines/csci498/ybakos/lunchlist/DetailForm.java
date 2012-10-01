8f5548d365c68a244bd7c8a53ead0b72ea390199
Yong Bakos <ybakos@humanoriented.com>
2012-10-01 06:55:24 -0600

    Moving old tab-based layouts to activity-specific layouts.

--- LunchList/src/edu/mines/csci498/ybakos/lunchlist/DetailForm.java
+++ LunchList/src/edu/mines/csci498/ybakos/lunchlist/DetailForm.java
@@ -2,11 +2,49 @@ package edu.mines.csci498.ybakos.lunchlist;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.database.Cursor;
+import android.view.*;
+import android.widget.*;
 
 public class DetailForm extends Activity {
 
+	EditText name;
+	EditText address;
+	EditText notes;
+	RadioGroup types;
+	RestaurantHelper helper;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.detail_form);
+		helper = new RestaurantHelper(this);
+
+		name = (EditText) findViewById(R.id.name);
+		address = (EditText) findViewById(R.id.address);
+		notes = (EditText) findViewById(R.id.notes);
+		types = (RadioGroup) findViewById(R.id.restaurantTypes);
+
+		Button save = (Button) findViewById(R.id.save);
+
+		save.setOnClickListener(onSave);
 	}
+	
+	private View.OnClickListener onSave = new View.OnClickListener() {
+		public void onClick(View v) {
+			String type = null;
+			switch (types.getCheckedRadioButtonId()) {
+			case R.id.sit_down:
+				type = "sit_down";
+				break;
+			case R.id.take_out:
+				type = "take_out";
+				break;
+			case R.id.delivery:
+				type = "delivery";
+				break;
+			}
+		}
+	};
+	
 }
--- LunchList/src/edu/mines/csci498/ybakos/lunchlist/LunchListActivity.java
+++ LunchList/src/edu/mines/csci498/ybakos/lunchlist/LunchListActivity.java
@@ -2,6 +2,7 @@ package edu.mines.csci498.ybakos.lunchlist;
 
 import android.app.TabActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.*;
@@ -84,20 +85,8 @@ public class LunchListActivity extends TabActivity {
 
 	private AdapterView.OnItemClickListener onListClick = new AdapterView.OnItemClickListener() {
 		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-			model.moveToPosition(position);
-			name.setText(helper.getName(model));
-			address.setText(helper.getAddress(model));
-			notes.setText(helper.getNotes(model));
-
-			if (helper.getType(model).equals("sit_down")) {
-				types.check(R.id.sit_down);
-			} else if (helper.getType(model).equals("take_out")) {
-				types.check(R.id.take_out);
-			} else {
-				types.check(R.id.delivery);
-			}
-
-			getTabHost().setCurrentTab(1);
+			Intent intent = new Intent(LunchListActivity.this, DetailForm.class);
+			startActivity(intent);
 		}
 	};
 
