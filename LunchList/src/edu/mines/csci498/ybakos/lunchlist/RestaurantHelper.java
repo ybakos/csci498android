6b78351823686059bca22b09b74ba17c2c9696c4
Yong Bakos <ybakos@humanoriented.com>
2012-10-03 10:11:33 -0600

    Adding rss feed url schema change and helper changes.

--- LunchList/src/edu/mines/csci498/ybakos/lunchlist/RestaurantHelper.java
+++ LunchList/src/edu/mines/csci498/ybakos/lunchlist/RestaurantHelper.java
@@ -8,7 +8,7 @@ import android.database.sqlite.SQLiteDatabase;
 
 class RestaurantHelper extends SQLiteOpenHelper {
 
-	private static final int SCHEMA_VERSION = 2;
+	private static final int SCHEMA_VERSION = 3;
 	private Context context;
 	
 	public RestaurantHelper(Context context) {
@@ -24,8 +24,10 @@ class RestaurantHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		if (newVersion == 2) {
+		if (oldVersion < 2 && newVersion >= 2) {
 			db.execSQL(context.getString(R.string.migration01_add_url_to_restaurants));
+		} else if (oldVersion < 3 && newVersion >= 3) {
+			db.execSQL(context.getString(R.string.migration02_add_feed_to_restaurants));
 		}
 	}
 
@@ -33,30 +35,32 @@ class RestaurantHelper extends SQLiteOpenHelper {
 		return (getReadableDatabase().rawQuery(context.getString(R.string.db_select_restaurants) + " " + orderBy, null));
 	}
 
-	public void insert(String name, String address, String type, String notes) {
-		ContentValues cv = new ContentValues();
-		cv.put("name", name);
-		cv.put("address", address);
-		cv.put("type", type);
-		cv.put("notes", notes);
-		getWritableDatabase().insert("restaurants", "name", cv);
-	}
-	
-	public void update(String id, String name, String address, String type, String notes) {
-		ContentValues cv = new ContentValues();
-		String[] args = {id};
-		cv.put("name", name);
-		cv.put("address", address);
-		cv.put("type", type);
-		cv.put("notes", notes);
-		getWritableDatabase().update("restaurants", cv, "_id=?", args);
-	}
-
 	public Cursor getById(String id) {
 		String[] args = {id};
 		return getReadableDatabase().rawQuery(context.getString(R.string.db_select_restaurant), args);
 	}
 	
+	public void insert(String name, String address, String type, String notes, String feed) {
+		ContentValues cv = new ContentValues();
+		cv.put("name", name);
+		cv.put("address", address);
+		cv.put("type", type);
+		cv.put("notes", notes);
+		cv.put("feed", feed);
+		getWritableDatabase().insert("restaurants", "name", cv);
+	}
+	
+	public void update(String id, String name, String address, String type, String notes, String feed) {
+		ContentValues cv = new ContentValues();
+		String[] args = {id};
+		cv.put("name", name);
+		cv.put("address", address);
+		cv.put("type", type);
+		cv.put("notes", notes);
+		cv.put("notes", feed);
+		getWritableDatabase().update("restaurants", cv, "_id=?", args);
+	}
+
 	public String getName(Cursor c) {
 		return c.getString(1);
 	}
@@ -71,6 +75,10 @@ class RestaurantHelper extends SQLiteOpenHelper {
 
 	public String getNotes(Cursor c) {
 		return c.getString(4);
+	}
+	
+	public String getFeed(Cursor c) {
+		return c.getString(6);
 	}
 	
 	// Just a stub, to illustrate launching a browser activity via uri.
