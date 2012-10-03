a0af34f94a0ea71ded14460d0b3d87bd2ad39825
Yong Bakos <ybakos@humanoriented.com>
2012-10-03 08:18:16 -0600

    Overriding onSaveInstanceState and onRestoreInstanceState of DetailForm to preserve state during rotation.

--- LunchList/src/edu/mines/csci498/ybakos/lunchlist/DetailForm.java
+++ LunchList/src/edu/mines/csci498/ybakos/lunchlist/DetailForm.java
@@ -16,25 +16,6 @@ public class DetailForm extends Activity {
 	RestaurantHelper helper;
 	String restaurantId;
 	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.detail_form);
-		helper = new RestaurantHelper(this);
-
-		name = (EditText) findViewById(R.id.name);
-		address = (EditText) findViewById(R.id.address);
-		notes = (EditText) findViewById(R.id.notes);
-		types = (RadioGroup) findViewById(R.id.restaurantTypes);
-
-		Button save = (Button) findViewById(R.id.save);
-		save.setOnClickListener(onSave);
-		
-		restaurantId = getIntent().getStringExtra(LunchListActivity.ID_EXTRA);
-		
-		if (restaurantId != null) load();
-	}
-	
 	private View.OnClickListener onSave = new View.OnClickListener() {
 		public void onClick(View v) {
 			Log.d("LUNCHLIST:", "onSave called");
@@ -62,6 +43,49 @@ public class DetailForm extends Activity {
 		}
 	};
 	
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.detail_form);
+		helper = new RestaurantHelper(this);
+
+		name = (EditText) findViewById(R.id.name);
+		address = (EditText) findViewById(R.id.address);
+		notes = (EditText) findViewById(R.id.notes);
+		types = (RadioGroup) findViewById(R.id.restaurantTypes);
+
+		Button save = (Button) findViewById(R.id.save);
+		save.setOnClickListener(onSave);
+		
+		restaurantId = getIntent().getStringExtra(LunchListActivity.ID_EXTRA);
+		
+		if (restaurantId != null) load();
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle state) {
+		super.onSaveInstanceState(state);
+		state.putString("name", name.getText().toString());
+		state.putString("address", address.getText().toString());
+		state.putString("notes", notes.getText().toString());
+		state.putInt("type", types.getCheckedRadioButtonId());
+	}
+	
+	@Override
+	public void onRestoreInstanceState(Bundle state) {
+		super.onRestoreInstanceState(state);
+		name.setText(state.getString("name"));
+		address.setText(state.getString("address"));
+		notes.setText(state.getString("notes"));
+		types.check(state.getInt("type"));
+	}
+	
+	public void onDestroy() {
+		super.onDestroy();
+		helper.close();
+	}
+
 	private void load() {
 		Cursor c = helper.getById(restaurantId);
 		c.moveToFirst();
@@ -80,9 +104,5 @@ public class DetailForm extends Activity {
 
 		c.close();
 	}
-	
-	public void onDestroy() {
-		super.onDestroy();
-		helper.close();
-	}
+
 }
