411f63206f619684080192545d9adca2639c32fe
Yong Bakos <ybakos@humanoriented.com>
2012-10-04 09:54:52 -0600

    Implementing actual handling of RSS data and preserving activity state for task.

--- LunchList/src/edu/mines/csci498/ybakos/lunchlist/FeedActivity.java
+++ LunchList/src/edu/mines/csci498/ybakos/lunchlist/FeedActivity.java
@@ -1,53 +1,124 @@
 package edu.mines.csci498.ybakos.lunchlist;
 
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSItem;
+import org.mcsoxford.rss.RSSReader;
 
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 public class FeedActivity extends ListActivity {
 
-	public static final String FEED_URL = "http://feeds.macrumors.com/MacRumors-All";
+	public static final String FEED_URL = "edu.mines.csci498.ybakos.lunchlist.FEED_URL";
+	private InstanceState state;
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		state = (InstanceState) getLastNonConfigurationInstance();
+		
+		if (state == null) {
+			state = new InstanceState();
+			state.task = new FeedTask(this);
+			state.task.execute(getIntent().getStringExtra(FEED_URL));
+		} else {
+			if (state.task != null) state.task.attach(this);
+			if (state.feed != null) setFeed(state.feed);
+		}
+	}
+	
+	@Override
+	public Object onRetainNonConfigurationInstance() {
+		if (state.task != null) state.task.detach();
+		return state;
+	}
+	
+	private void setFeed(RSSFeed feed) {
+		state.feed = feed;
+		setListAdapter(new FeedAdapter(feed));
+	}
 	
 	private void goBlooey(Throwable t) {
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		builder.setTitle("Exception").setMessage(t.toString()).setPositiveButton("Ok", null).show();
 	}
 	
-	private static class FeedTask extends AsyncTask<String, Void, Void> {
+	private static class InstanceState {
+		RSSFeed feed;
+		FeedTask task;
+	}
+	
+	private class FeedAdapter extends BaseAdapter {
+		RSSFeed feed = null;
 		
+		public FeedAdapter(RSSFeed feed) {
+			super();
+			this.feed = feed;
+		}
+		
+		public int getCount() {
+			return feed.getItems().size();
+		}
+		
+		public Object getItem(int position) {
+			return feed.getItems().get(position);
+		}
+		
+		public long getItemId(int position) {
+			return position;
+		}
+		
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View row = convertView;
+			if (row == null) {
+				LayoutInflater inflater = getLayoutInflater();
+				row = inflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+			}
+			RSSItem item = (RSSItem) getItem(position);
+			((TextView) row).setText(item.getTitle());
+			return row;
+		}
+		
+	}
+	
+	private static class FeedTask extends AsyncTask<String, Void, RSSFeed> {
+		
+		private RSSReader reader;
 		private Exception ex;
 		private FeedActivity activity;
 		
 		FeedTask(FeedActivity activity) {
+			reader = new RSSReader();
 			attach(activity);
 		}
 		
 		@Override
-		public Void doInBackground(String... urls) {
+		public RSSFeed doInBackground(String... urls) {
+			RSSFeed result = null;
 			try {
-				DefaultHttpClient client = new DefaultHttpClient();
-				HttpGet getMethod = new HttpGet(urls[0]);
-				ResponseHandler<String> responseHandler = new BasicResponseHandler();
-				String responseBody = client.execute(getMethod, responseHandler);
-				Log.d("FeedActivity", responseBody);
+				result = reader.load(urls[0]);
 			} catch (Exception e) {
 				this.ex = e;
 			}
-			return null;
+			return result;
 		}
 		
 		@Override
-		public void onPostExecute(Void unused) {
+		public void onPostExecute(RSSFeed feed) {
 			if (ex == null) {
-				// Don't swallow exceptions. This comment does not exist. I heart a paradox.
+				activity.setFeed(feed);
 			} else {
-				Log.e("LunchList", "Exception parsing feed: " + ex);
+				Log.e("LunchList", "Exception parsing feed: ", ex);
 				activity.goBlooey(ex); // Tell the activity that we blooeyed. (sp?)
 			}
 		}
@@ -55,6 +126,11 @@ public class FeedActivity extends ListActivity {
 		private void attach(FeedActivity activity) {
 			this.activity = activity;
 		}
+		
+		private void detach() {
+			this.activity = null;
+		}
+		
 	}
 	
 }
