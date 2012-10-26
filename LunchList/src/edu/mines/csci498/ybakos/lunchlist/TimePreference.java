11052d0f22084bd6d07f3040d0116f325a6a339a
Yong Bakos <ybakos@humanoriented.com>
2012-10-26 08:54:34 -0600

    Adding TimePreference implementation.

---
+++ LunchList/src/edu/mines/csci498/ybakos/lunchlist/TimePreference.java
@@ -0,0 +1,74 @@
+package edu.mines.csci498.ybakos.lunchlist;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TimePicker;
+
+public class TimePreference extends DialogPreference {
+
+	private int lastHour;
+	private int lastMinute;
+	private TimePicker picker;
+	
+	public static int getHour(String time) {
+		String[] pieces = time.split(":");
+		return Integer.parseInt(pieces[0]);
+	}
+	
+	public static int getMinute(String time) {
+		String[] pieces = time.split(":");
+		return Integer.parseInt(pieces[1]);
+	}
+	
+	public TimePreference(Context context, AttributeSet attributes) {
+		super(context, attributes);
+		setPositiveButtonText("Set");
+		setNegativeButtonText("Cancel");
+	}
+	
+	@Override
+	protected View onCreateDialogView() {
+		picker = new TimePicker(getContext());
+		return picker;
+	}
+	
+	@Override
+	protected void onBindDialogView(View v) {
+		super.onBindDialogView(v);
+		picker.setCurrentHour(lastHour);
+		picker.setCurrentMinute(lastMinute);
+	}
+	
+	@Override
+	protected void onDialogClosed(boolean result) {
+		super.onDialogClosed(result);
+		if (result) {
+			lastHour = picker.getCurrentHour();
+			lastMinute = picker.getCurrentMinute();
+			
+			String time = String.valueOf(lastHour) + ":" + String.valueOf(lastMinute);
+			
+			if (callChangeListener(time)) persistString(time);
+		}
+	}
+	
+	@Override
+	protected Object onGetDefaultValue(TypedArray a, int index) {
+		return a.getString(index);
+	}
+	
+	@Override
+	protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
+		String time = null;
+		if (restoreValue) {
+			if (defaultValue == null) time = getPersistedString("00:00");
+			else time = getPersistedString(defaultValue.toString());
+		} else time = defaultValue.toString();
+		lastHour = getHour(time);
+		lastMinute = getMinute(time);
+	}
+	
+}
