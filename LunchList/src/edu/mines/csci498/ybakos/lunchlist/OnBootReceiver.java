97cda0ee3618e63455480ab038e3080188c82838
Yong Bakos <ybakos@humanoriented.com>
2012-10-26 09:17:46 -0600

    Adding stub OnBootReceiver implementation and manifest entry.

---
+++ LunchList/src/edu/mines/csci498/ybakos/lunchlist/OnBootReceiver.java
@@ -0,0 +1,16 @@
+package edu.mines.csci498.ybakos.lunchlist;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class OnBootReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		
+	}
+	
+	
+	
+}
