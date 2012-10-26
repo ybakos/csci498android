7d60c577d971526632c47c77e32bd94216865ec8
Yong Bakos <ybakos@humanoriented.com>
2012-10-26 11:21:48 -0600

    Adding layout and class definition for AlarmActivity.

---
+++ LunchList/src/edu/mines/csci498/ybakos/lunchlist/AlarmActivity.java
@@ -0,0 +1,14 @@
+package edu.mines.csci498.ybakos.lunchlist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AlarmActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.alarm);
+	}
+	
+}
