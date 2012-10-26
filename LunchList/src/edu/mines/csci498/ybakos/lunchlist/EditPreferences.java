070cb7e15d1da564739522b53988ee28b9c1a029
Yong Bakos <ybakos@humanoriented.com>
2012-10-26 11:16:49 -0600

    Enhancing EditPreferences to manage alarm setting changes.

--- LunchList/src/edu/mines/csci498/ybakos/lunchlist/EditPreferences.java
+++ LunchList/src/edu/mines/csci498/ybakos/lunchlist/EditPreferences.java
@@ -1,10 +1,15 @@
 package edu.mines.csci498.ybakos.lunchlist;
 
-import android.app.Activity;
+import android.content.ComponentName;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
 public class EditPreferences extends PreferenceActivity {
+	
+	SharedPreferences preferences;
 	
 	@SuppressWarnings("deprecation")
 	@Override
@@ -12,5 +17,37 @@ public class EditPreferences extends PreferenceActivity {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
 	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		preferences = PreferenceManager.getDefaultSharedPreferences(this);
+		preferences.registerOnSharedPreferenceChangeListener(onChange);
+	}
+	
+	@Override
+	public void onPause() {
+		preferences.unregisterOnSharedPreferenceChangeListener(onChange);
+		super.onPause();
+	}
+	
+	SharedPreferences.OnSharedPreferenceChangeListener onChange = new SharedPreferences.OnSharedPreferenceChangeListener() {
+		
+		public void onSharedPreferenceChanged(SharedPreferences preferences, String key) {
+			if ("alarm".equals(key)) {
+				boolean enabled = preferences.getBoolean(key, false);
+				int flag = (enabled ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED : PackageManager.COMPONENT_ENABLED_STATE_DISABLED);
+				ComponentName component = new ComponentName(EditPreferences.this, OnBootReceiver.class);
+				
+				getPackageManager().setComponentEnabledSetting(component, flag, PackageManager.DONT_KILL_APP);
+				if (enabled) OnBootReceiver.setAlarm(EditPreferences.this);
+				else OnBootReceiver.cancelAlarm(EditPreferences.this);
+			} else if ("alarm_time".equals(key)) {
+				OnBootReceiver.cancelAlarm(EditPreferences.this);
+				OnBootReceiver.setAlarm(EditPreferences.this);
+			}
+		}
+		
+	};
 	
 }
