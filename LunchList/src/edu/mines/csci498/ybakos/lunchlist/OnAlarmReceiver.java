07c25bdb8600dfa6e29f220e7eed6f27d9a525e3
Yong Bakos <ybakos@humanoriented.com>
2012-10-31 07:07:56 -0600

    Leveraging more Notification.Builder methods and adding sound and good vibrations.

--- LunchList/src/edu/mines/csci498/ybakos/lunchlist/OnAlarmReceiver.java
+++ LunchList/src/edu/mines/csci498/ybakos/lunchlist/OnAlarmReceiver.java
@@ -7,6 +7,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.preference.PreferenceManager;
 
 public class OnAlarmReceiver extends BroadcastReceiver {
@@ -23,13 +25,16 @@ public class OnAlarmReceiver extends BroadcastReceiver {
 	private void generateNotification(Context context) {
 		PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, new Intent(context, AlarmActivity.class), 0);
 		NotificationManager manager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+//		Uri uri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 		Notification notice = new Notification.Builder(context)
 			.setTicker("Time for a drink OMG squeeal!")
 			.setContentTitle(context.getString(R.string.alarmMessage))
 			.setSmallIcon(R.drawable.ic_popup_reminder)
 			.setContentText(context.getString(R.string.app_name))
-			.setContentIntent(pendingIntent).getNotification();
-		notice.flags |= Notification.FLAG_AUTO_CANCEL;
+//			.setSound(uri)
+			.setContentIntent(pendingIntent)
+			.setAutoCancel(true)
+			.setDefaults(Notification.DEFAULT_VIBRATE | Notification.DEFAULT_SOUND).getNotification();
 		manager.notify(NOTIFY_ME_ID, notice);
 	}
 	
