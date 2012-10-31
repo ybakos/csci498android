d6247fa8bba894d3da50b2d622ca5b2d6a22cc8b
Yong Bakos <ybakos@humanoriented.com>
2012-10-31 06:54:42 -0600

    Refactoring to methods and Notification.Builder.

--- LunchList/src/edu/mines/csci498/ybakos/lunchlist/OnAlarmReceiver.java
+++ LunchList/src/edu/mines/csci498/ybakos/lunchlist/OnAlarmReceiver.java
@@ -16,24 +16,27 @@ public class OnAlarmReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-		
-		if (preferences.getBoolean("use_notification", true)) {
-			NotificationManager manager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-			Notification notice = new Notification(R.drawable.ic_popup_reminder,
-												   context.getString(R.string.alarmMessage),
-												   System.currentTimeMillis());
-			PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, new Intent(context, AlarmActivity.class), 0);
-			notice.setLatestEventInfo(context,
-									  context.getString(R.string.app_name),
-									  context.getString(R.string.alarmMessage),
-									  pendingIntent);
-			notice.flags |= Notification.FLAG_AUTO_CANCEL;
-			manager.notify(NOTIFY_ME_ID, notice);
-		} else {
-			Intent alarmIntent = new Intent(context, AlarmActivity.class);
-			alarmIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // Necessary when calling startActivity outside of an Activity.
-			context.startActivity(alarmIntent);
-		}
+		if (preferences.getBoolean("use_notification", true)) generateNotification(context);
+		else showAlarmActivity(context);
+	}
+	
+	private void generateNotification(Context context) {
+		PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, new Intent(context, AlarmActivity.class), 0);
+		NotificationManager manager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+		Notification notice = new Notification.Builder(context)
+			.setTicker("Time for a drink OMG squeeal!")
+			.setContentTitle(context.getString(R.string.alarmMessage))
+			.setSmallIcon(R.drawable.ic_popup_reminder)
+			.setContentText(context.getString(R.string.app_name))
+			.setContentIntent(pendingIntent).getNotification();
+		notice.flags |= Notification.FLAG_AUTO_CANCEL;
+		manager.notify(NOTIFY_ME_ID, notice);
+	}
+	
+	private void showAlarmActivity(Context context) {
+		Intent alarmIntent = new Intent(context, AlarmActivity.class);
+		alarmIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // Necessary when calling startActivity outside of an Activity.
+		context.startActivity(alarmIntent);
 	}
 	
 }
