7aefc53d82f36e256494b1fedacb8320c23aed00
Yong Bakos <ybakos@humanoriented.com>
2012-11-05 09:33:03 -0700

    Adding WidgetService implementation and dependencies.

--- LunchList/src/edu/mines/csci498/ybakos/lunchlist/LunchListWidget.java
+++ LunchList/src/edu/mines/csci498/ybakos/lunchlist/LunchListWidget.java
@@ -1,7 +1,15 @@
 package edu.mines.csci498.ybakos.lunchlist;
 
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
 
 public class LunchListWidget extends AppWidgetProvider {
 
+	@Override
+	public void onUpdate(Context context, AppWidgetManager manager, int[] appWidgetIds) {
+		context.startService(new Intent(context, WidgetService.class));
+	}
+	
 }
---
+++ LunchList/src/edu/mines/csci498/ybakos/lunchlist/WidgetService.java
@@ -0,0 +1,54 @@
+package edu.mines.csci498.ybakos.lunchlist;
+
+import android.app.IntentService;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.database.Cursor;
+import android.widget.RemoteViews;
+
+public class WidgetService extends IntentService {
+
+	public WidgetService() {
+		super("WidgetService");
+	}
+	
+	@Override
+	public void onHandleIntent(Intent intent) {
+		ComponentName me = new ComponentName(this, LunchListWidget.class);
+		RemoteViews updateViews = new RemoteViews("edu.mines.csci498.ybakos.lunchlist", R.layout.widget);
+		RestaurantHelper helper = new RestaurantHelper(this);
+		AppWidgetManager manager = AppWidgetManager.getInstance(this);
+		
+		try {
+			Cursor cursor = helper.getReadableDatabase().rawQuery("SELECT COUNT(*) FROM restaurants", null);
+			cursor.moveToFirst();
+			int count = cursor.getInt(0);
+			cursor.close();
+			
+			if (count > 0) {
+				int offset = (int) (count * Math.random());
+				String args[] = {String.valueOf(offset)};
+				cursor = helper.getReadableDatabase().rawQuery("SELECT _ID, name FROM restaurants LIMIT 1 OFFSET ?", args);
+				cursor.moveToFirst();
+				updateViews.setTextViewText(R.id.name, cursor.getString(1));
+				Intent detailFormIntent = new Intent(this, DetailForm.class);
+				detailFormIntent.putExtra(LunchListActivity.ID_EXTRA, cursor.getString(0));
+				PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, detailFormIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+				updateViews.setOnClickPendingIntent(R.id.name, pendingIntent);
+				cursor.close();
+			} else {
+				updateViews.setTextViewText(R.id.name, this.getString(R.string.widget_empty));
+			}
+		} finally {
+			helper.close();
+		}
+		Intent widgetIntent = new Intent(this, WidgetService.class);
+		PendingIntent pendingIntent = PendingIntent.getService(this, 0, widgetIntent, 0);
+		updateViews.setOnClickPendingIntent(R.id.next, pendingIntent);
+		manager.updateAppWidget(me, updateViews);
+		
+	}
+	
+}
