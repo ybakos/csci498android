ad60812dc10484c8ae05e2beb5280cb35006a4bb
Yong Bakos <ybakos@humanoriented.com>
2012-11-14 12:03:02 -0700

    Implementing logic for loading the detail fragment from within LunchListActivity.

--- LunchList/src/edu/mines/csci498/ybakos/lunchlist/LunchListActivity.java
+++ LunchList/src/edu/mines/csci498/ybakos/lunchlist/LunchListActivity.java
@@ -3,6 +3,8 @@ package edu.mines.csci498.ybakos.lunchlist;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
 public class LunchListActivity extends FragmentActivity implements LunchListFragment.OnRestaurantListener {
 	
@@ -18,9 +20,24 @@ public class LunchListActivity extends FragmentActivity implements LunchListFrag
 	}
 	
 	public void onRestaurantSelected(long id) {
-		Intent intent = new Intent(this, DetailForm.class);
-		intent.putExtra(ID_EXTRA, String.valueOf(id));
-		startActivity(intent);
+		if (findViewById(R.id.details) == null) {
+			Intent intent = new Intent(this, DetailForm.class);
+			intent.putExtra(ID_EXTRA, String.valueOf(id));
+			startActivity(intent);
+		} else {
+			FragmentManager fragmentManager = getSupportFragmentManager();
+			DetailFragment detailFragment = (DetailFragment) fragmentManager.findFragmentById(R.id.details);
+			if (detailFragment == null) {
+				detailFragment = DetailFragment.newInstance(id);
+				FragmentTransaction tx = fragmentManager.beginTransaction();
+				tx.add(R.id.details, detailFragment)
+				  .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+				  .addToBackStack(null)
+				  .commit();
+			} else {
+				detailFragment.loadRestaurant(String.valueOf(id));
+			}
+		}
 	}
 	
 }
