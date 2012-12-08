52e149a5b967e219a478498fa1d514076344e72a
Yong Bakos <ybakos@humanoriented.com>
2012-12-07 17:11:17 -0700

    Adding audio playback of alarm sound to AlarmActivity.

--- LunchList/src/edu/mines/csci498/ybakos/lunchlist/AlarmActivity.java
+++ LunchList/src/edu/mines/csci498/ybakos/lunchlist/AlarmActivity.java
@@ -1,14 +1,46 @@
 package edu.mines.csci498.ybakos.lunchlist;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 
-public class AlarmActivity extends Activity {
+public class AlarmActivity extends Activity implements MediaPlayer.OnPreparedListener {
 
+	MediaPlayer player;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		player = new MediaPlayer();
 		setContentView(R.layout.alarm);
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+		String sound = preferences.getString("alarm_ringtone", null);
+		if (sound != null) {
+			player.setAudioStreamType(AudioManager.STREAM_ALARM);
+			try {
+				player.setDataSource(sound);
+				player.setOnPreparedListener(this);
+				player.prepareAsync();
+			} catch (Exception e) {
+				Log.e("LunchList", "Exception in playing ringtone", e);
+			}
+		}
+	}
+	
+	@Override
+	public void onPause() {
+		if (player.isPlaying()) {
+			player.stop();
+		}
+		super.onPause();
+	}
+	
+	public void onPrepared(MediaPlayer player) {
+		player.start();
 	}
 	
 }
